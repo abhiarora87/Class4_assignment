6d74c450bedd7d14f237af24d40d89b8102549ac
Abhishek Arora <aarora@arccorp.com>
2019-09-24 21:29:56 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class4_assignment
+{
+    class Colleague : Person
+    {
+
+        public Colleague DeptNum;
+
+        /// <summary>
+        /// Default constructor attempt 1
+        /// </summary>
+        public Colleague()
+        {
+            string DeptNum = "A12";
+        }
+
+        /// <summary>
+        /// Default constructor that uses inheritance
+        /// </summary>
+        //public Student() : base()
+        //{
+        //  studentNumber = "U1234";
+        //}
+    }
+}
